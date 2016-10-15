94f3ccb5f9ba81f787a239680ab0d8414c853437
Aerendir <hello@aerendir.me>
2016-10-15 12:14:12 +0200

    Test `StripeLocalWebhookEvent`.

---
+++ Tests/Model/StripeLocalWebhookEventTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace SerendipityHQ\Bundle\StripeBundle\Tests\Model;
+
+use SerendipityHQ\Bundle\StripeBundle\Model\StripeLocalCustomer;
+use SerendipityHQ\Bundle\StripeBundle\Model\StripeLocalWebhookEvent;
+use SerendipityHQ\Bundle\StripeBundle\Tests\ModelTestCase;
+
+/**
+ * Tests the StripeLocalWebhookEvent entity.
+ *
+ * @author Adamo Aerendir Crespi <hello@aerendir.me>
+ */
+class StripeLocalWebhookEventTest extends ModelTestCase
+{
+    public function testStripeLocalWebhookEvent()
+    {
+        $resource = new StripeLocalWebhookEvent();
+
+        $expected = [
+            'data' => '{"json": {"representation": "of data"}}',
+        ];
+
+        // Test setMethods
+        $resource->setData($expected['data']);
+
+        $this::assertSame($expected['data'], $resource->getData());
+
+        $expectedData = [
+            'created' => $this::createMock(\DateTime::class),
+            'id' => 'cus_customeridisastring',
+            'pendingWebhooks' => 2,
+            'request' => 'unknown',
+            'type' => 'event.type',
+            'livemode' => false
+        ];
+
+        // Populate the object
+        $this->populateModel($resource, $expectedData);
+
+        $this::assertSame($expectedData['created'], $resource->getCreated());
+        $this::assertSame($expectedData['id'], $resource->getId());
+        $this::assertSame($expectedData['id'], $resource->__toString());
+        $this::assertSame($expectedData['pendingWebhooks'], $resource->getPendingWebhooks());
+        $this::assertSame($expectedData['request'], $resource->getRequest());
+        $this::assertSame($expectedData['type'], $resource->getType());
+        $this::assertFalse($resource->isLivemode());
+
+        $this::expectException(\BadMethodCallException::class);
+        $resource->toStripe('');
+    }
+
+    public function testSetNewSourceThrowsAnExceptionWithAnInvalidTokenString()
+    {
+        $resource = new StripeLocalCustomer();
+
+        $this::expectException(\InvalidArgumentException::class);
+        $resource->setNewSource('notok_sourceid');
+    }
+
+    public function testToStringThrowsAnExceptionWithAnInvalidAction()
+    {
+        $resource = new StripeLocalCustomer();
+
+        $this::expectException(\InvalidArgumentException::class);
+        $resource->toStripe('not_create_or_update');
+    }
+}
