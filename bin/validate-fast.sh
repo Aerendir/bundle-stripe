1fedea11981750bfb5e80d79a598b51d05e64f1a
Aerendir <hello@aerendir.me>
2020-07-06 10:01:10 +0200

    Update `bin` commands.

--- bin/validate-fast.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-    vendor/bin/security-checker security:check &&
-    tools/php-cs-fixer fix --allow-risky yes --dry-run -v &&
-    tools/phpstan analyse src -c phpstan.neon --level 2 &&
-    tools/phan --progress-bar --minimum-severity 2
