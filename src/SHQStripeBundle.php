b7612a8a81fc2207308a193ab3cba575b2d9b729
Aerendir <hello@aerendir.me>
2020-07-22 11:21:29 +0200

    Update config of PHP CS Fixer.

--- src/SHQStripeBundle.php
+++ src/SHQStripeBundle.php
@@ -1,16 +1,12 @@
 <?php
 
 /*
- * This file is part of the SHQStripeBundle.
+ * This file is part of the Serendipity HQ Stripe Bundle.
  *
- * Copyright Adamo Aerendir Crespi 2016-2017.
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * @author    Adamo Aerendir Crespi <hello@aerendir.me>
- * @copyright Copyright (C) 2016 - 2017 Aerendir. All rights reserved.
- * @license   MIT License.
  */
 
 namespace SerendipityHQ\Bundle\StripeBundle;
