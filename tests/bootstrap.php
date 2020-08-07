ea0abd8d9090a2b22717929e76ab1a492d007a89
Aerendir <hello@aerendir.me>
2020-08-07 10:34:19 +0200

    Make tests run.

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Serendipity HQ Aws Ses Bundle.
+ *
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use DG\BypassFinals;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+BypassFinals::enable();
