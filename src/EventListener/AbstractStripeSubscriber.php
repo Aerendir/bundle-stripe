39c02d81a6b681281b1acfa20c5a9393c4f452f6
Aerendir <hello@aerendir.me>
2020-08-11 15:12:58 +0200

    Fix interface `EventSubscriberInterface`.

--- src/EventListener/AbstractStripeSubscriber.php
+++ src/EventListener/AbstractStripeSubscriber.php
@@ -12,7 +12,7 @@
 namespace SerendipityHQ\Bundle\StripeBundle\EventListener;
 
 use SerendipityHQ\Bundle\StripeBundle\Service\StripeManager;
-use Symfony\Contracts\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Manages Charges on Stripe.
