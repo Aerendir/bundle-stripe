67a308b0864fa37e52cfffc2f09196ea61c645eb
Aerendir <hello@aerendir.me>
2021-12-08 12:58:25 +0100

    Update Rector and PHPStan.

--- rector.php
+++ rector.php
@@ -18,72 +18,25 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
         __DIR__ . '/tests'
     ]);
 
-    $parameters->set(Option::AUTOLOAD_PATHS, [
-        __DIR__ . '/vendor-bin/phpunit/vendor/autoload.php',
-    ]);
+    $parameters->set(Option::BOOTSTRAP_FILES, [__DIR__ . '/vendor-bin/phpunit/vendor/autoload.php']);
 
-    $parameters->set(
-        Option::SETS,
-        [
-            SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
-            SetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
-            SetList::CODE_QUALITY,
-            SetList::CODING_STYLE,
-            SetList::MONOLOG_20,
-            SetList::SWIFTMAILER_60,
-            SetList::DOCTRINE_25,
-            SetList::DOCTRINE_CODE_QUALITY,
-            SetList::DOCTRINE_SERVICES,
-            SetList::DOCTRINE_DBAL_210,
-            SetList::FRAMEWORK_EXTRA_BUNDLE_40,
-            SetList::FRAMEWORK_EXTRA_BUNDLE_50,
-            SetList::PHP_52,
-            SetList::PHP_53,
-            SetList::PHP_54,
-            SetList::PHP_56,
-            SetList::PHP_70,
-            SetList::PHP_71,
-            SetList::PHP_72,
-            SetList::PHPUNIT_40,
-            SetList::PHPUNIT_50,
-            SetList::PHPUNIT_60,
-            SetList::PHPUNIT_70,
-            SetList::PHPUNIT_75,
-            SetList::PHPUNIT_80,
-            SetList::PHPUNIT80_DMS,
-            SetList::PHPUNIT_CODE_QUALITY,
-            SetList::PHPUNIT_EXCEPTION,
-            SetList::PHPUNIT_MOCK,
-            SetList::PHPUNIT_SPECIFIC_METHOD,
-            SetList::PHPUNIT_YIELD_DATA_PROVIDER,
-            SetList::UNWRAP_COMPAT,
-            SetList::SYMFONY_26,
-            SetList::SYMFONY_28,
-            SetList::SYMFONY_30,
-            SetList::SYMFONY_31,
-            SetList::SYMFONY_32,
-            SetList::SYMFONY_33,
-            SetList::SYMFONY_34,
-            SetList::SYMFONY_40,
-            SetList::SYMFONY_41,
-            SetList::SYMFONY_42,
-            SetList::SYMFONY_43,
-            SetList::SYMFONY_44,
-            SetList::SYMFONY_50,
-            SetList::SYMFONY_50_TYPES,
-            SetList::SYMFONY_CODE_QUALITY,
-            SetList::SYMFONY_CONSTRUCTOR_INJECTION,
-            SetList::SAFE_07,
-            SetList::TYPE_DECLARATION,
-            SetList::TWIG_112,
-            SetList::TWIG_127,
-            SetList::TWIG_134,
-            SetList::TWIG_140,
-            SetList::TWIG_20,
-            SetList::TWIG_240,
-            SetList::TWIG_UNDERSCORE_TO_NAMESPACE,
-        ]
-    );
+    $containerConfigurator->import(SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::CODING_STYLE);
+    $containerConfigurator->import(SetList::MONOLOG_20);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
+    $containerConfigurator->import(SetList::PHP_52);
+    $containerConfigurator->import(SetList::PHP_53);
+    $containerConfigurator->import(SetList::PHP_54);
+    $containerConfigurator->import(SetList::PHP_56);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+    $containerConfigurator->import(SetList::PHP_73);
+    $containerConfigurator->import(SetList::UNWRAP_COMPAT);
+    $containerConfigurator->import(SetList::SAFE_07);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION);
 
     $parameters->set(
         Option::SKIP,
