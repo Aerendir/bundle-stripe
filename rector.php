0a75ad2e6d664a4d662b8a385a2e82f2b047087c
Aerendir <hello@aerendir.me>
2022-05-28 16:29:04 +0200

    Update Rector's config.

--- rector.php
+++ rector.php
@@ -2,16 +2,13 @@
 
 declare(strict_types = 1);
 
+use Rector\Config\RectorConfig;
 use SerendipityHQ\Integration\Rector\SerendipityHQ;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 
-return static function (ContainerConfigurator $containerConfigurator) : void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
-    $parameters->set(Option::PATHS, [
+return static function (RectorConfig $rectorConfig) : void {
+    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->paths([
         __DIR__ . '/dev',
         __DIR__ . '/src',
         // This causes issues with controllers
@@ -21,10 +18,9 @@ return static function (ContainerConfigurator $containerConfigurator) : void {
 
     // This causes issues with controllers
     // Until required for tests, keep it commented
-    //$parameters->set(Option::BOOTSTRAP_FILES, [__DIR__ . '/vendor-bin/phpunit/vendor/autoload.php']);
-
-    $containerConfigurator->import(SerendipityHQ::SHQ_SYMFONY_BUNDLE);
+    // $rectorConfig->bootstrapFiles([__DIR__ . '/vendor-bin/phpunit/vendor/autoload.php']);
+    $rectorConfig->import(SerendipityHQ::SHQ_SYMFONY_BUNDLE);
 
     $toSkip = SerendipityHQ::buildToSkip(SerendipityHQ::SHQ_SYMFONY_BUNDLE_SKIP);
-    $parameters->set(Option::SKIP, $toSkip);
+    $rectorConfig->skip($toSkip);
 };
