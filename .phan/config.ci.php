ac750ff1be1111832ab8d4c00f0dc9d5c9f53682
Aerendir <hello@aerendir.me>
2022-07-19 08:32:15 +0200

    Use `serendipity_hq/phpcs-config`.

--- .phan/config.ci.php
+++ .phan/config.ci.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Serendipity HQ Stripe Bundle.
+ *
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return array_merge(require_once 'config.php', [
     'suppress_issue_types' => [
         'PhanRedefinedClassReference',
--- rector.php
+++ rector.php
@@ -1,12 +1,21 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
+
+/*
+ * This file is part of the Serendipity HQ Stripe Bundle.
+ *
+ * Copyright (c) Adamo Aerendir Crespi <aerendir@serendipityhq.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Rector\Config\RectorConfig;
-use SerendipityHQ\Integration\Rector\SerendipityHQ;
 use Rector\Core\ValueObject\PhpVersion;
+use SerendipityHQ\Integration\Rector\SerendipityHQ;
 
-return static function (RectorConfig $rectorConfig) : void {
+return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->phpVersion(PhpVersion::PHP_74);
     $rectorConfig->paths([
         __DIR__ . '/dev',
